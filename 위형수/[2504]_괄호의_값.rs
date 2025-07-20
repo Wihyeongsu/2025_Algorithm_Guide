613e4b5a7e89f53388d3db0e5bc7174ca5c74a83
Wihyeongsu <162963431+Wihyeongsu@users.noreply.github.com>
2025-07-20 08:04:39 +0000

    [BOJ] 괄호의 값 / 2504 / 골드5 / 0주차-자료구조 / rust

--- 위형수/[2504]_괄호의_값.rs
+++ 위형수/[2504]_괄호의_값.rs
@@ -1,97 +1,8 @@
 use std::io::Read;
 
-// 괄호를 쌍으로 가지므로 길이가 짝수
-// 한 쌍의 괄호를 child로 가지는 트리 형성
-
-#[derive(Clone, Copy, Debug, PartialEq)]
-enum Token{
-    Parenthesis(char),
-    SquareBracket(char),
-    Eof
-}
-
-struct Lexer{
-    tokens:Vec<Token>
-}
-
-impl Lexer {
-    fn new(input: &str) -> Lexer {
-        let mut tokens = input
-        .chars()
-        .map(|c| match c {
-            '('|')' => Token::Parenthesis(c),
-            '['|']' => Token::SquareBracket(c),
-            _ => unreachable!()
-        }).collect::<Vec<_>>();
-        tokens.reverse();
-
-        Lexer{ tokens }
-    }
-
-    fn next(&mut self) -> Token{
-        self.tokens.pop().unwrap_or(Token::Eof)
-    }
-
-    fn peek(&self) -> Token{
-        self.tokens.last().copied().unwrap_or(Token::Eof)
-    }
-}
-
-fn parse(lexer: &mut Lexer, recent_token: &Token) -> (Token, usize){
-    let mut sum = 0;
-    loop {
-        let lhs = match lexer.next() {
-            Token::Eof => return (Token::Eof, sum), // End
-            t @ Token::Parenthesis(')') => {
-                if *recent_token == Token::Parenthesis('('){
-                    return (t, sum);
-                }else {
-                    return (Token::Eof, 0);
-                }
-            },
-            t @ Token::SquareBracket(']') => {
-                if *recent_token == Token::SquareBracket('['){
-                    return (t, sum);
-                }else {
-                    return (Token::Eof, 0);
-                }
-            },
-            t => t,
-        };
-        let rhs= match lexer.peek() {
-            Token::Parenthesis('(') | Token::SquareBracket('[') => {
-                let rhs = match parse(lexer, &lhs) {
-                    (t @ Token::Parenthesis(')'), v) => {
-                        sum += 2 * v;
-                        t
-                    },
-                    (t @ Token::SquareBracket(']'), v) =>{
-                        sum += 3 * v;
-                        t
-                    },
-                    _ => return (Token::Eof, 0), // upto different pair result
-                };
-                rhs
-            },
-            t @ Token::Parenthesis(')') => {
-                lexer.next(); // consume
-                sum += 2;
-                t
-            } 
-            t @ Token::SquareBracket(']') => {
-                lexer.next(); // consume
-                sum += 3;
-                t
-            } 
-            t => return (t, 0)
-        };
-
-
-        // different pair
-        if !((lhs == Token::Parenthesis('(') && rhs == Token::Parenthesis(')')) 
-        || (lhs == Token::SquareBracket('[') && rhs == Token::SquareBracket(']'))) {return (Token::Eof, 0);}
-    }
-}
+// 깊이가 커질수록 곱하기
+// 한 쌍의 괄호일 때 값을 계산
+// 깊이가 작아질 때 나누기
 
 fn main() {
     let input = {
@@ -100,16 +11,41 @@ fn main() {
         buf
     };
     let input = input.trim();
-    // println!("Input: {:?}", input);
+    
+    let mut stack:Vec<char> = Vec::new();
+    let mut temp = 1;
+    let mut former_token = '#'; // init any
 
-    let result = if input.len() % 2 == 0 {
-        let mut lexer = Lexer::new(input);
-        let (_, result) = parse(&mut lexer, &Token::Eof);
-        result
-    } else{
-        0
-    };
+    let mut result = input.chars().fold(0, |mut acc, token| 
+        {
+            match token {
+                '(' => {
+                    temp *= 2;
+                    stack.push(token);
+                },
+                '[' => {
+                    temp *= 3;
+                    stack.push(token);
+                },
+                ')' if stack.last() == Some(&'(') => {
+                    if former_token == '(' { acc += temp; }
+                    temp /= 2;
+                    stack.pop();
+                }
+                ']' if stack.last() == Some(&'[') => {
+                    if former_token == '[' { acc += temp; }
+                    temp /= 3;
+                    stack.pop();
+                },
+                _ => { acc = 0; temp = 0; }
+            };
 
-    println!("{result}");
+            former_token = token;
 
+            acc
+        }
+    );
+
+    if !stack.is_empty() { result = 0; }
+    println!("{result}")
 }

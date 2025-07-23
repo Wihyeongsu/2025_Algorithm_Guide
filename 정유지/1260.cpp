d15e6e25e1747da37793710585db3c8e3b837dd4
yuji4 <165403706+yuji4@users.noreply.github.com>
2025-07-23 23:07:04 +0900

    [BOJ] DFS와 BFS / 1260 / 실버2 / 1주차-DFS/BFS / cpp

--- 정유지/1260.cpp
+++ 정유지/1260.cpp
@@ -3,8 +3,8 @@
 #include <algorithm>
 using namespace std;
 
-vector<vector<int>> graph;  // 인접 리스트 벡터
-vector<bool> visited;  // 방문 여부 벡터
+vector<vector<int>> graph;
+vector<bool> visited;  
 
 // 깊이 우선 탐색 함수
 void dfs(int v){
